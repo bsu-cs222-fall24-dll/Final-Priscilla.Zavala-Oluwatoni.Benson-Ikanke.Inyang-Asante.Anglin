8a3529223c977d028608a929ce8aa4c468654074
pazavala <priscillazav27@gmail.com>
2024-10-18 14:35:26 -0400

    Small change in order

--- src/test/java/edu/bsu/cs/UserModelTest.java
+++ src/test/java/edu/bsu/cs/UserModelTest.java
@@ -9,21 +9,21 @@ public class UserModelTest {
     @Test
     void testGetPositionCredentialCostAnalyst() {
         UserModel userModel = new UserModel();
-        String result = userModel.getPositionCredential("100");
-        assertEquals(result, "Cost Analyst");
+        String costAnalystResult = userModel.getPositionCredential("100");
+        assertEquals("Cost Analyst", costAnalystResult);
     }
 
     @Test
     void testGetPositionCredentialAuditor() {
         UserModel userModel = new UserModel();
-        String result = userModel.getPositionCredential("200");
-        assertEquals(result, "Auditor");
+        String auditorResult = userModel.getPositionCredential("200");
+        assertEquals("Auditor", auditorResult);
     }
 
     @Test
     void testGetPositionCredentialInvalidCredential() {
         UserModel userModel = new UserModel();
-        String result = userModel.getPositionCredential("");
-        assertNull(result, "Expects null for invalid or empty credential ID");
+        String invalidResult = userModel.getPositionCredential("");
+        assertNull(invalidResult, "Expects null for invalid or empty credential ID");
     }
 }
