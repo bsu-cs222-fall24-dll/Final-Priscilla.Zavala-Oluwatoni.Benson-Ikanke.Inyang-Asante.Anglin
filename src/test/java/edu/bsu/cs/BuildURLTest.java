fa573dc7f0d70e259617af30d08c6ba1c42cbf42
pazavala <priscillazav27@gmail.com>
2024-10-18 14:34:53 -0400

    Create and pass build URL unit tests

--- src/test/java/edu/bsu/cs/BuildURLTest.java
+++ src/test/java/edu/bsu/cs/BuildURLTest.java
@@ -1,15 +1,24 @@
 package edu.bsu.cs;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BuildURLTest {
     @Test
-    public void testStateURL() {
-
+    public void testBuildHospitalsInStateURL() {
+        String expectedHospitalsInStateURL = "https://www.communitybenefitinsight.org/api/get_hospitals.php?state=OH";
+        String testState = "OH";
+        BuildURL testStateURL = new BuildURL();
+        String outputURL = testStateURL.buildHospitalsInStateURL(testState);
+        Assertions.assertEquals(expectedHospitalsInStateURL, outputURL);
     }
 
     @Test
-    public void testHospitalURL() {
-
+    public void testBuildHospitalDataURL() {
+        String expectedHospitalDataURL = "https://www.communitybenefitinsight.org/api/get_hospital_data.php?hospital_id=2303";
+        String testHospitalID = "2303";
+        BuildURL testHospitalDataURL = new BuildURL();
+        String outputURL = testHospitalDataURL.buildHospitalDataURL(testHospitalID);
+        Assertions.assertEquals(expectedHospitalDataURL, outputURL);
     }
 }
