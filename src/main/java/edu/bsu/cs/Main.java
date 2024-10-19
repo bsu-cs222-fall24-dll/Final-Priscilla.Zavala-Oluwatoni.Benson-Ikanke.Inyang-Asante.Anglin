9a398a41fd2f417f7c28315b962b8f70f5aad537
Ikanke Inyang <ikanke04@gmail.com>
2024-10-19 15:29:52 -0400

    created WebsiteURLConnection class and ErrorProcessor class

--- src/main/java/edu/bsu/cs/Main.java
+++ src/main/java/edu/bsu/cs/Main.java
@@ -5,17 +5,15 @@ import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws MalformedURLException, URISyntaxException {
-//        UserModel model = new UserModel();
-//        UserView view = new UserView();
-//        MenuController controller = new MenuController(model, view);
-//        controller.runMenu();
-//
-//        String state = UserInput.stateInput();
-//        BuildURL buildURL = new BuildURL();
-//        System.out.println("List of hospitals in state: " + buildURL.buildHospitalsInStateURL(state));
-//        String hospital = UserInput.hospitalIDInput();
-//        System.out.println("List of hospitals in state: " + buildURL.buildHospitalDataURL(hospital));
+        UserModel model = new UserModel();
+        UserView view = new UserView();
+        MenuController controller = new MenuController(model, view);
+        controller.runMenu();
 
-        WebsiteURLConnection.connectHospitalURL("10");
+        String state = UserInput.stateInput();
+        BuildURL buildURL = new BuildURL();
+        System.out.println("List of hospitals in state: " + buildURL.buildHospitalsInStateURL(state));
+        String hospital = UserInput.hospitalIDInput();
+        System.out.println("List of hospitals in state: " + buildURL.buildHospitalDataURL(hospital));
     }
 }
