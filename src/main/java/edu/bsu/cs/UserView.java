1b062607a6f296cbcdf57cbc12adfd0c6ee9fc73
pazavala <priscillazav27@gmail.com>
2024-10-19 13:58:12 -0400

    Add tasks to display for each position

--- src/main/java/edu/bsu/cs/UserView.java
+++ src/main/java/edu/bsu/cs/UserView.java
@@ -9,11 +9,33 @@ public class UserView {
                 """);
     }
 
-    public void displayWelcomeMessage(String message) {
+    protected void displayCostAnalystTasks() {
+        System.out.println("""
+                Position Tasks Specified Below:
+                - Total Revenue ID: 101
+                - Total Functional Expenses ID: 102
+                - Subsidized Health Services as a % of Total Functional Expenses: 103
+                - Health Professions Education as a % of Total Functional Expenses: 104
+                - Total Community Benefits Costs: 105
+                - Charity Care Expenses: 106
+                - Community Health Improvement Services & Community Benefit Operations Cost: 107
+                """);
+    }
+
+    protected void displayAuditorTasks() {
+        System.out.println("""
+                Position Tasks Specified Below:
+                - Bad debt as a % of Total Functional Expenses ID: 201
+                - General Bad Debt ID: 202
+                """);
+    }
+
+    public void displayMessage(String message) {
         System.out.println(message);
     }
 
     public void displayErrorMessage(String errorMessage) {
         System.err.println(errorMessage);
     }
+
 }
