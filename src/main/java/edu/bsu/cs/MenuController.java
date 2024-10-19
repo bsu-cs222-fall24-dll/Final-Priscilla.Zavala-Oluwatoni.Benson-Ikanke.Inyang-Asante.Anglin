9f066e804ca0ed553440b95b393d29a84d0504dd
pazavala <priscillazav27@gmail.com>
2024-10-19 14:03:14 -0400

    Add position task menu and process user input while keeping model-view code separated

--- src/main/java/edu/bsu/cs/MenuController.java
+++ src/main/java/edu/bsu/cs/MenuController.java
@@ -11,14 +11,48 @@ public class MenuController {
 
     protected void runMenu() {
         view.displayPositionOptions();
-
         String credentialID = UserInput.credentialInput();
         if (model.isValidCredential(credentialID)) {
             String welcomeRoleMessage = model.getPositionCredential(credentialID);
-            view.displayWelcomeMessage("Now logged in as a " + welcomeRoleMessage + "!");
+            view.displayMessage("Now logged in as a " + welcomeRoleMessage + "!");
+            runPositionTaskMenu(credentialID);
         } else {
             view.displayErrorMessage("Invalid credential. Please try again.");
             runMenu();
+        }
+    }
+
+    protected void runPositionTaskMenu(String credentialID) {
+        String position = model.getPositionCredential(credentialID);
+        if (position != null) {
+            displayTasksBasedOnPosition(position);
+            processUserTaskInput(position);
+        }
+    }
+
+    private void displayTasksBasedOnPosition(String position) {
+        if (position.equals("Cost Analyst")) {
+            view.displayCostAnalystTasks();
+        } else if (position.equals("Auditor")) {
+            view.displayAuditorTasks();
+        }
+    }
+
+    private void processUserTaskInput(String position) {
+        String taskID = UserInput.taskIDInput();
+        boolean isValidPositionTask = false;
+
+        if (position.equals("Cost Analyst")) {
+            isValidPositionTask = model.isValidCostAnalystSpecification(taskID);
+        } else if (position.equals("Auditor")) {
+            isValidPositionTask = model.isValidAuditorSpecification(taskID);
+        }
+
+        if (isValidPositionTask) {
+            view.displayMessage("Valid " + position + " task selected: " + taskID);
+        } else {
+            view.displayErrorMessage("Invalid " + position + " task. Please try again.");
+            processUserTaskInput(position);
         }
     }
 }
