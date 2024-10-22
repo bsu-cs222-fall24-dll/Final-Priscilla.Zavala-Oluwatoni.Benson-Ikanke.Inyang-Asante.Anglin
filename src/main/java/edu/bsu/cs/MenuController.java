a0d29607a1274ed12b9eabe116d4542a96d7ec0b
pazavala <priscillazav27@gmail.com>
2024-10-22 10:36:50 -0400

    Add hospital info menu method to build URL and display from view - call method in main menu

--- src/main/java/edu/bsu/cs/MenuController.java
+++ src/main/java/edu/bsu/cs/MenuController.java
@@ -3,10 +3,12 @@ package edu.bsu.cs;
 public class MenuController {
     private final UserModel model;
     private final UserView view;
+    private final BuildURL buildURL;
 
-    protected MenuController(UserModel model, UserView view) {
+    protected MenuController(UserModel model, UserView view, BuildURL buildURL) {
         this.model = model;
         this.view = view;
+        this.buildURL = buildURL;
     }
 
     protected void runMenu() {
@@ -20,6 +22,7 @@ public class MenuController {
             view.displayErrorMessage("Invalid credential. Please try again.");
             runMenu();
         }
+
     }
 
     protected void runPositionTaskMenu(String credentialID) {
@@ -50,9 +53,20 @@ public class MenuController {
 
         if (isValidPositionTask) {
             view.displayMessage("Valid " + position + " task selected: " + taskID);
+            runHospitalInfoMenu();
         } else {
             view.displayErrorMessage("Invalid " + position + " task. Please try again.");
             processUserTaskInput(position);
         }
+    }
+
+    private void runHospitalInfoMenu() {
+        String state = UserInput.stateInput();
+        String hospitalsInStateURL = buildURL.buildHospitalsInStateURL(state);
+        view.displayMessage("List of hospitals in state: " + hospitalsInStateURL);
+
+        String hospitalID = UserInput.hospitalIDInput();
+        String hospitalDataURL = buildURL.buildHospitalDataURL(hospitalID);
+        view.displayMessage("Data for hospital ID: " + hospitalDataURL);
     }
 }
