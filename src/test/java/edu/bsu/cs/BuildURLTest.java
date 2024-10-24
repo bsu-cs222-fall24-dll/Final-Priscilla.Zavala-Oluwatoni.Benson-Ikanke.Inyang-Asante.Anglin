b12f96360c4db28ba2c3e1163608e9b65f3b28ab
pazavala <priscillazav27@gmail.com>
2024-10-24 09:21:17 -0400

    Add unit tests for more coverage with refactor input validators

--- src/test/java/edu/bsu/cs/BuildURLTest.java
+++ src/test/java/edu/bsu/cs/BuildURLTest.java
@@ -1,24 +1,58 @@
 package edu.bsu.cs;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BuildURLTest {
+    private BuildURL buildURL;
+
+    @BeforeEach
+    public void setUp() {
+        buildURL = new BuildURL();
+    }
+
     @Test
-    public void testBuildHospitalsInStateURL() {
+    public void testIsValidState_EmptyString() {
+        assertFalse(buildURL.isValidState(""));
+    }
+
+    @Test
+    public void testIsValidState_ValidState() {
+        assertTrue(buildURL.isValidState("CA"));
+    }
+
+    @Test
+    public void testIsValidState_InvalidState() {
+        assertFalse(buildURL.isValidState("ZZ"));
+    }
+
+    @Test
+    public void testBuildStateHospitalURL() {
         String expectedHospitalsInStateURL = "https://www.communitybenefitinsight.org/api/get_hospitals.php?state=OH";
         String testState = "OH";
-        BuildURL testStateURL = new BuildURL();
-        String outputURL = testStateURL.buildHospitalsInStateURL(testState);
-        Assertions.assertEquals(expectedHospitalsInStateURL, outputURL);
+        String outputURL = buildURL.buildStateHospitalURL(testState);
+        assertEquals(expectedHospitalsInStateURL, outputURL);
+    }
+
+    @Test
+    public void testBuildHospitalDataURL_EmptyHospitalID() {
+        String expectedURL = "https://www.communitybenefitinsight.org/api/get_hospital_data.php?hospital_id=";
+        assertEquals(expectedURL, buildURL.buildHospitalDataURL(""));
+    }
+
+    @Test
+    public void testBuildHospitalDataURL_NonNumericHospitalID() {
+        String expectedURL = "https://www.communitybenefitinsight.org/api/get_hospital_data.php?hospital_id=abc123";
+        assertEquals(expectedURL, buildURL.buildHospitalDataURL("abc123"));
     }
 
     @Test
     public void testBuildHospitalDataURL() {
         String expectedHospitalDataURL = "https://www.communitybenefitinsight.org/api/get_hospital_data.php?hospital_id=2303";
         String testHospitalID = "2303";
-        BuildURL testHospitalDataURL = new BuildURL();
-        String outputURL = testHospitalDataURL.buildHospitalDataURL(testHospitalID);
-        Assertions.assertEquals(expectedHospitalDataURL, outputURL);
+        String outputURL = buildURL.buildHospitalDataURL(testHospitalID);
+        assertEquals(expectedHospitalDataURL, outputURL);
     }
 }
