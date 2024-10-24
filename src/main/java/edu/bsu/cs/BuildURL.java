0c4d8d5b592d2307a63ef762209312a5744e114f
pazavala <priscillazav27@gmail.com>
2024-10-23 23:37:46 -0400

    Update BuildURL to use StateAbbreviation enum for validation

--- src/main/java/edu/bsu/cs/BuildURL.java
+++ src/main/java/edu/bsu/cs/BuildURL.java
@@ -1,8 +1,25 @@
 package edu.bsu.cs;
 
 public class BuildURL {
+    public String stateValidatorAndBuildURL() {
+        while (true) {
+            String state = UserInput.stateInput();
+            try {
+                return buildStateHospitalURL(state);
+            } catch (IllegalArgumentException e) {
+                UserView error = new UserView();
+                error.displayErrorMessage(e.getMessage());
+            }
+        }
+    }
+
     public String buildStateHospitalURL(String state) {
-        return "https://www.communitybenefitinsight.org/api/get_hospitals.php?state=" + state;
+        try {
+            StateAbbreviation.valueOf(state);
+            return "https://www.communitybenefitinsight.org/api/get_hospitals.php?state=" + state;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Please enter a valid state abbreviation.");
+        }
     }
 
     public String buildHospitalDataURL(String hospital) {
