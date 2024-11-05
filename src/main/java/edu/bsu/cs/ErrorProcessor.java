0124718ba629afc362b8d9bd09592b3b0b6db66d
Ikanke Inyang <ikanke04@gmail.com>
2024-11-04 20:31:43 -0500

    edit ErrorProcessor for global error messages

--- src/main/java/edu/bsu/cs/ErrorProcessor.java
+++ src/main/java/edu/bsu/cs/ErrorProcessor.java
@@ -6,6 +6,10 @@ import java.net.URL;
 public final class ErrorProcessor {
     UserView errorMessage = new UserView();
 
+    public static void displayErrorMessage(String message) {
+        System.err.println(message);
+    }
+
     public void checkNetworkConnection(URL url) {
         try{
             url.openConnection().connect();
