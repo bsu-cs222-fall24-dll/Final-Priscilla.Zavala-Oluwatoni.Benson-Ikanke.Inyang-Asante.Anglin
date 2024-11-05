61e59bf900d80560bf13e6eaab500439f8cf9f21
Ikanke Inyang <ikanke04@gmail.com>
2024-11-04 21:11:37 -0500

    edit MenuController to make cost analyst and auditor print out values

--- src/main/java/edu/bsu/cs/MenuController.java
+++ src/main/java/edu/bsu/cs/MenuController.java
@@ -1,12 +1,19 @@
 package edu.bsu.cs;
 
-import java.util.List;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
-public class MenuController {
+public class MenuController extends UserView{
     private final UserModel model;
     private final UserView view;
     private final BuildURL buildURL;
-
+    public CostAnalystModel costAnalystModel = new CostAnalystModel();
+    public CostAnalystView costAnalystView = new CostAnalystView();
+    public TaskOptions taskOptions = new TaskOptions();
+    public AuditorData auditorData = new AuditorData();
+    public String taskID = "";
+    private String jsonPath;
+    private String jsonFile;
 
     protected MenuController(UserModel model, UserView view, BuildURL buildURL) {
         this.model = model;
@@ -14,7 +21,7 @@ public class MenuController {
         this.buildURL = buildURL;
     }
 
-    protected void runMenu() {
+    protected void runMenu() throws IOException, URISyntaxException {
         view.displayPositionOptions();
         String credentialID = UserInput.credentialInput();
         if (model.isValidCredential(credentialID)) {
@@ -28,19 +35,31 @@ public class MenuController {
 
     }
 
-    protected void runPositionTaskMenu(String credentialID) {
+    protected void runPositionTaskMenu(String credentialID) throws IOException, URISyntaxException {
         String position = model.getPositionByCredential(credentialID);
         if (position != null) {
-            TaskOptions taskOptions = new TaskOptions();
-            List<String> taskList = taskOptions.getTasksForPosition(position);
-            view.displayTasksForPosition(taskList);
+            displayTasksBasedOnPosition(position);
             processUserTaskInput(position);
         }
     }
 
-    private void processUserTaskInput(String position) {
-        String taskID = UserInput.taskIDInput();
-        boolean isValidPositionTask = model.isValidTaskForPosition(position, taskID);
+    private void displayTasksBasedOnPosition(String position) {
+        if (position.equals("Cost Analyst")) {
+            view.displayTasksForPosition(taskOptions.getTasksForPosition("Cost Analyst"));
+        } else if (position.equals("Auditor")) {
+            //view.displayAuditorTasks();
+        }
+    }
+
+    private void processUserTaskInput(String position) throws IOException, URISyntaxException {
+        taskID = UserInput.taskIDInput();
+        boolean isValidPositionTask = false;
+
+        if (position.equals("Cost Analyst")) {
+            isValidPositionTask = model.isValidCostAnalystSpecification(taskID);
+        } else if (position.equals("Auditor")) {
+            isValidPositionTask = model.isValidAuditorSpecification(taskID);
+        }
 
         if (isValidPositionTask) {
             view.displayMessage("Valid " + position + " task selected: " + taskID);
@@ -51,11 +70,21 @@ public class MenuController {
         }
     }
 
-    private void runHospitalInfoMenu() {
-        String hospitalsInStateURL = buildURL.stateValidatorBuildURL();
+    private void runHospitalInfoMenu() throws IOException, URISyntaxException {
+        String state = UserInput.stateInput();
+        String hospitalsInStateURL = buildURL.buildStateHospitalURL(state);
         view.displayMessage("List of hospitals in state: " + hospitalsInStateURL);
 
-        String hospitalDataURL = buildURL.hospitalValidatorBuildURL();
+        String hospitalID = UserInput.hospitalIDInput();
+        String hospitalDataURL = buildURL.buildHospitalDataURL(hospitalID);
         view.displayMessage("Data for hospital ID: " + hospitalDataURL);
+        //menuDisplay(hospitalID);
+        auditorData.retrieveHospitalData(hospitalID, "bad_debt_tot_func_exp_pct");
+    }
+
+    private void menuDisplay(String hospitalID) throws IOException, URISyntaxException {
+        jsonPath = costAnalystModel.retrieveJsonPath(taskID);
+        jsonFile = costAnalystModel.retrieveJsonFile(hospitalID, jsonPath);
+        costAnalystView.displayFormattedData(jsonFile, jsonPath);
     }
 }
