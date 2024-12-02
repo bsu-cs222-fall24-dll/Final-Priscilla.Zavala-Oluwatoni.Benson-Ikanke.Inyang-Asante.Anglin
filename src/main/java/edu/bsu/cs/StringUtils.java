bd28f8409e5f36f85c1afafa65355e5e41e9b490
pazavala <priscillazav27@gmail.com>
2024-12-01 20:31:58 -0500

    Add utility method with regex pattern to search for hospital - code reusability since position controllers follows this logic

--- src/main/java/edu/bsu/cs/StringUtils.java
+++ src/main/java/edu/bsu/cs/StringUtils.java
@@ -1,5 +1,8 @@
 package edu.bsu.cs;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class StringUtils {
     //Helper method
     static boolean isNumeric(String str) {
@@ -9,5 +12,22 @@ public final class StringUtils {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    public static String extractFirstNumber(String str) {
+        //handle null and/or empty strings
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+
+        //handle specified patterns
+        String regex = "\\d+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        return null;
     }
 }
