8072b60e5b512d4958ab7c6720ed451cbf92f76f
pazavala <priscillazav27@gmail.com>
2024-12-04 14:12:38 -0500

    Add unit tests to cover rest of model code in Medicaid Data Analyst class

--- src/test/java/edu/bsu/cs/MedicaidDataAnalystModelTest.java
+++ src/test/java/edu/bsu/cs/MedicaidDataAnalystModelTest.java
@@ -29,6 +29,16 @@ public class MedicaidDataAnalystModelTest {
     }
 
     @Test
+    void testRetrieveJsonPath_ValidTaskID404() {
+        assertEquals("unreim_medcd", medicaidDataAnalystModel.retrieveJsonPath("404"));
+    }
+
+    @Test
+    void testRetrieveJsonPath_ValidTaskID405() {
+        assertEquals("rat_pat_care_npat_care_comm_bnfts", medicaidDataAnalystModel.retrieveJsonPath("405"));
+    }
+
+    @Test
     void testRetrieveJsonPath_InvalidTaskID() {
         String result = medicaidDataAnalystModel.retrieveJsonPath("999");
         assertEquals("", result);
@@ -44,6 +54,24 @@ public class MedicaidDataAnalystModelTest {
     void testChangeTitle_ValidJsonPathTitle2() {
         medicaidDataAnalystModel.changeTitle("mdcre_shrtfl");
         assertEquals("Medicare Shortfall (Negative Value Indicates Surplus)", medicaidDataAnalystModel.getTitle());
+    }
+
+    @Test
+    void testChangeTitle_ValidJsonPathTitle3() {
+        medicaidDataAnalystModel.changeTitle("mdcre_shrtfl_tot_func_exp_pct");
+        assertEquals("Medicare Shortfall as % of Total Functional Expenses (Negative Value Indicates Surplus)", medicaidDataAnalystModel.getTitle());
+    }
+
+    @Test
+    void testChangeTitle_ValidJsonPathTitle4() {
+        medicaidDataAnalystModel.changeTitle("unreim_medcd");
+        assertEquals("Unreimbursed Medicaid", medicaidDataAnalystModel.getTitle());
+    }
+
+    @Test
+    void testChangeTitle_ValidJsonPathTitle5() {
+        medicaidDataAnalystModel.changeTitle("rat_pat_care_npat_care_comm_bnfts");
+        assertEquals("Ratio of Patient Care to Non-patient Care Community Benefits", medicaidDataAnalystModel.getTitle());
     }
 
     @Test
