dccc97d8ce5a44034a039f97c263727937f97753
pazavala <priscillazav27@gmail.com>
2024-12-05 22:58:24 -0500

    Note in comment - unable to separate controller logic from business logic due to program requiring both logics to be in same class for functionality

--- src/main/java/edu/bsu/cs/ExportCSV.java
+++ src/main/java/edu/bsu/cs/ExportCSV.java
@@ -3,25 +3,27 @@ package edu.bsu.cs;
 import java.io.File;
 import java.io.FileWriter;
 
+//known class handles business logic and controller flow - UNABLE TO REFACTOR TO SEPARATE
+//attempt to refactor class to separate into model and controller, error consistently occurs with exporting
 public class ExportCSV {
+    static UserView view = new UserView();
 
     public static void exportToCSV(PositionModel positionModel, String[] hospitalData, String[] hospitalYear){
-
         String directoryPath = "output/ExportedCSV/";
         File directory = new File(directoryPath);
+        view.displayMessage("Creating directory at: " + directory.getAbsolutePath());
 
-        System.out.println("Creating directory at: " + directory.getAbsolutePath());
-
+        //cannot create unit tests as requires direct access to user directory
         if (!directory.exists()) {
             boolean dirsCreated = directory.mkdirs();
             if (!dirsCreated) {
                 AlertUtils.showError("Failed to create directories: " + directoryPath);
-                return; // Exit the method if directory creation failed
+                return;
             }
         }
 
+        //cannot write user test as requires user input to retrieve hospital name & year
         String fileName = AlertUtils.showDialogBox("");
-
         try (FileWriter writer = new FileWriter(directoryPath + fileName + ".csv")) {
             writer.write("Hospital Name, Year, Task, Data\n");
             for(int i = 0; i < hospitalData.length; i++){
